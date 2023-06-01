31e8ca619335f7fc6f63715f68a043ae8c792858
anowereng <anowereng@gmail.com>
2023-06-02 01:51:17 +0600

    ..

--- SalesCartSystem/SalesCartSystem/Queues/QuueueClient.cs
+++ SalesCartSystem/SalesCartSystem/Queues/QuueueClient.cs
@@ -47,7 +47,7 @@ namespace SaleCartFunction
             // Get the connection string from app settings
             string connectionString = "UseDevelopmentStorage=true";//ConfigurationManager.AppSettings["StorageConnectionString"];
 
-            // Instantiate a QueueClient which will be used to create and manipulate the queue
+            // Instantiate a QueueClient which will be used to create and manipulate the queue   
             QueueClient queueClient = new QueueClient(connectionString, queueName);
 
             // Create the queue if it doesn't already exist
